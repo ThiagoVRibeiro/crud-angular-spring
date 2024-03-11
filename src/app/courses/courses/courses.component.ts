67d8d17d060bf6b262c3944ac4f76f5777435258
Thiago Ribeiro <thvinicius1@gmail.com>
2024-03-11 06:55:52 -0300

    Aula 6 e 7 - Criando Material Table e Módulo do App Material

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
 
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'FrontEnd'}
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor(){
+    //this.courses = []; outra maneira de iniciar o course
+   }
+  ngOnInit(): void {
+
+  }
 }
